b28148f73bfbe95067b2a20e02fa006bfc75e85a
Lenar <leonruscraft@gmail.com>
2020-10-20 15:41:42 +0300

    feat: solve task 70%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.flat(); 
+    let stack = []
+  
+    for(let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket)
+      if(bracket === '|') {
+        continue;
+      }
+      if(bracketsIndex % 2 === 0 ) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
 }
